53ca03ed5287a8a5379d1bdbeedca2e0865ee0c0
Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
2021-02-10 21:14:56 -0500

    chore: Initial Configuration (#1)
    * chore(webapp): Create React App and Switch to NPM
    
    * chore(functions): Configure Firebase Functions, Hosting, Emulator, Security Rules, etc.
    
    * chore(types): Configure Shared Data Types
    
    * chore: Install Material UI, Firebase, and Prettier Dependencies

---
+++ @types/index.d.ts
@@ -0,0 +1,3 @@
+declare interface User {
+  stuff: string;
+}
---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from "firebase-functions";
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  functions.logger.info(process.env.FIREBASE_CONFIG as string, {
+    structuredData: true,
+  });
+  response.send("Hello from Firebase!");
+});
