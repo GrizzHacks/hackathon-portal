008f623778698affae15d6ef6edd1e5e6af8a547
Shriyash Jalukar <shriyash1111@gmail.com>
2021-02-17 21:23:02 -0500

    work in progress: post request

--- functions/src/modules/stpm/companies/createCompany.ts
+++ functions/src/modules/stpm/companies/createCompany.ts
@@ -1,8 +1,30 @@
 import type { ExpressFunction } from "../../../@types";
+import { firebaseApp } from "../../../config/firebaseConfig";
+import { expressErrorHandlerFactory } from "../../../helpers";
+import { uasPermissionSwitch } from "../../../systems/uas";
+
+
+
 
 const createCompany: ExpressFunction = (req, res, next) => {
-  res.status(200).send();
-  next();
+  uasPermissionSwitch({
+    organizer: { accepted: execute },
+  })(req, res, next);
 };
+
+const execute: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+  firebaseApp
+    .firestore()
+    .collection("sponsorCompany")
+    .doc(req.params.companyId)
+    .set(req.body)
+    .then(() => {
+      res.status(201).send();
+      next();
+    })
+    .catch(errorHandler);
+};
+
 
 export default createCompany;
