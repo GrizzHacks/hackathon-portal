dd81b04eb682297c6b36a636394d249083e6cd9f
Andrew Dimmer <contactandrewd@gmail.com>
2021-03-02 19:14:10 -0500

    feat(functions): Created a Type Validator to Parse JSON Request Bodies
    Note: the type validator is thus far untested, and tests will need to be defined for it!

---
+++ @types/helper/typeValidator.d.ts
@@ -0,0 +1,27 @@
+declare interface ValidatorAdvancedTypeRules {
+  type: "array" | "object";
+}
+
+declare interface ValidatorObjectRules extends ValidatorAdvancedTypeRules {
+  rules: {
+    [key: string]: { rules: ValidatorAllowedTypes; required?: boolean };
+  };
+  type: "object";
+}
+
+declare interface ValidatorArrayRules extends ValidatorAdvancedTypeRules {
+  rules: ValidatorAllowedTypes;
+  type: "array";
+}
+
+declare type ValidatorAllowedTypes = (primitive | ValidatorAdvancedTypeRules)[];
+
+declare type primitive =
+  | "string"
+  | "number"
+  | "bigint"
+  | "symbol"
+  | "null"
+  | "undefined"
+  | "boolean"
+  | "function";
