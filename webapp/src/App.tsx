6ae6ec6aecdf45e4341ce9e005c115e9c66610bc
Andrew Dimmer <contactandrewd@gmail.com>
2021-03-04 05:59:19 -0500

    feat(webapp): Added Top Navigation Bar

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/layouts/NavBar";
 import BugReportFab from "./components/misc/BugReportFab";
 import ApiExplorer from "./devTools/ApiExplorer";
 import Home from "./components/Home";
@@ -13,6 +14,12 @@ declare interface AppProps {
 const App: React.FunctionComponent<AppProps> = ({ theme, toggleTheme }) => {
   return (
     <Fragment>
+      <NavBar
+        theme={theme}
+        toggleTheme={toggleTheme}
+        currentUserProfile={null}
+        pageTitle=""
+      />
       <Router>
         <div className="App">
           <Switch>
