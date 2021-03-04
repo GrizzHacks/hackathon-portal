db23360bd1eb6688075c6d8989ce619b5e989501
Andrew Dimmer <contactandrewd@gmail.com>
2021-03-03 21:55:58 -0500

    feat: Create STPM Tiers Type Definitions

---
+++ @types/modules/stpm/tiers.d.ts
@@ -0,0 +1,61 @@
+declare interface STPMTierUpdateRequest {
+  sponsorTierName?: string;
+  sponsorTierOrder?: number;
+  logoSize?: "xs" | "sm" | "m" | "lg" | "xl";
+  sponsorshipExpo?: boolean;
+  techTalk?: boolean;
+  officeHours?: boolean;
+  prize?: boolean;
+  prizeBudget?: number;
+  attendeeData?: "none" | "pre" | "post";
+  numberOfMentors?: number;
+  numberOfRecruiters?: number;
+  distributionOfSwag?: boolean;
+  openingSessionTalk?: boolean;
+  openingSessionTalkLength?: number;
+  closingSessionTalk?: boolean;
+  closingSessionTalkLength?: number;
+  otherBenefits?: { [key: string]: string };
+}
+
+declare interface STPMTierCreateRequest {
+  sponsorTierId: string;
+  sponsorTierName: string;
+  sponsorTierOrder?: number;
+  logoSize: "xs" | "sm" | "m" | "lg" | "xl";
+  sponsorshipExpo: boolean;
+  techTalk: boolean;
+  officeHours: boolean;
+  prize: boolean;
+  prizeBudget?: number;
+  attendeeData: "none" | "pre" | "post";
+  numberOfMentors: number;
+  numberOfRecruiters: number;
+  distributionOfSwag: boolean;
+  openingSessionTalk: boolean;
+  openingSessionTalkLength?: number;
+  closingSessionTalk: boolean;
+  closingSessionTalkLength?: number;
+  otherBenefits?: { [key: string]: string };
+}
+
+declare interface STPMTier {
+  sponsorTierId: string;
+  sponsorTierName: string;
+  sponsorTierOrder: number;
+  logoSize: "xs" | "sm" | "m" | "lg" | "xl";
+  sponsorshipExpo: boolean;
+  techTalk: boolean;
+  officeHours: boolean;
+  prize: boolean;
+  prizeBudget: number;
+  attendeeData: "none" | "pre" | "post";
+  numberOfMentors: number;
+  numberOfRecruiters: number;
+  distributionOfSwag: boolean;
+  openingSessionTalk: boolean;
+  openingSessionTalkLength: number;
+  closingSessionTalk: boolean;
+  closingSessionTalkLength: number;
+  otherBenefits: { [key: string]: string };
+}
