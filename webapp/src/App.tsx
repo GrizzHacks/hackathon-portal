b4aa40f0b4bf08237fccb1ffc2647bf2ddb94e4b
Andrew Dimmer <contactandrewd@gmail.com>
2021-03-04 05:55:16 -0500

    feat(webapp): Added Bug Report Button
    Note: This could be confused for debug tools, so this might change...

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BugReportFab from "./components/misc/BugReportFab";
 import ApiExplorer from "./devTools/ApiExplorer";
 import Home from "./components/Home";
 
@@ -11,15 +12,18 @@ declare interface AppProps {
 
 const App: React.FunctionComponent<AppProps> = ({ theme, toggleTheme }) => {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/api" component={ApiExplorer} />
-          <Route render={() => <div>404 Not Found</div>} />
-        </Switch>
-      </div>
-    </Router>
+    <Fragment>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/api" component={ApiExplorer} />
+            <Route render={() => <div>404 Not Found</div>} />
+          </Switch>
+        </div>
+      </Router>
+      <BugReportFab />
+    </Fragment>
   );
 };
 
