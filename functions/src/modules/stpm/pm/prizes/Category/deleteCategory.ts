5b41c1c2a475a840b92e9484f6b739f5b849778a
nicksinishtaj <nicholassinistaj@gmail.com>
2021-03-04 01:58:23 -0500

    create endpoints for deleting a normal category and listing categories

---
+++ functions/src/modules/stpm/pm/prizes/Category/deleteCategory.ts
@@ -0,0 +1,29 @@
+import type { ExpressFunction } from "../../../../../@types";
+import { firebaseApp } from "../../../../../config/firebaseConfig";
+import { expressErrorHandlerFactory } from "../../../../../helpers";
+import { uasPermissionSwitch } from "../../../../../systems/uas";
+
+const deleteCategory: ExpressFunction = (req, res, next) => {
+  res.status(200).send();
+  next();
+  uasPermissionSwitch({
+    organizer: { accepted: execute },
+    sponsor: { accepted: execute },
+  })(req, res, next);
+};
+
+const execute: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+  firebaseApp
+    .firestore()
+    .collection("prizeCategories")
+    .doc(req.params.tierId)
+    .delete()
+    .then(() => {
+      res.status(201).send();
+      next();
+    })
+    .catch(errorHandler);
+};
+
+export default deleteCategory;
