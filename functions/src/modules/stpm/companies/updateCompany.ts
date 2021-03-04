fa615fee20effd2d92c42007b781043e4736fcab
Shriyash Jalukar <shriyash1111@gmail.com>
2021-03-04 01:07:09 -0500

    feat: work in progress updateCompany validation

--- functions/src/modules/stpm/companies/updateCompany.ts
+++ functions/src/modules/stpm/companies/updateCompany.ts
@@ -1,35 +1,47 @@
 import type { ExpressFunction } from "../../../@types";
 import { firebaseApp } from "../../../config/firebaseConfig";
-import { expressErrorHandlerFactory } from "../../../helpers";
+import {
+  expressErrorHandlerFactory,
+  requestBodyTypeValidator,
+} from "../../../helpers";
 import { uasPermissionSwitch } from "../../../systems/uas";
-
-
 
 const updateCompany: ExpressFunction = (req, res, next) => {
   uasPermissionSwitch({
-    organizer: { accepted: execute },
+    organizer: { accepted: validateOrganizer },
     sponsor: { accepted: executeIfSponsorMatches },
   })(req, res, next);
 };
 
+const validateOrganizer: ExpressFunction = (req, res, next) => {
+  const validationRules: ValidatorObjectRules = {
+    type: "object",
+    rules: {
+      companyName: { rules: ["string"] },
+      companyLogoUrl: { rules: ["string"] },
+      companyAcronym: { rules: ["string"] },
+      sponsorTierId: { rules: ["string"] },
+
+      //overriddenBenefits: { [key: string]: string }; TODO: Support Dictionary Advanced Types
+    },
+  };
+  requestBodyTypeValidator(req, res, next)(validationRules, execute);
+};
+
 const execute: ExpressFunction = (req, res, next) => {
-
   const errorHandler = expressErrorHandlerFactory(req, res, next);
-
-  const body = JSON.parse(req.body)
 
   firebaseApp
     .firestore()
     .collection("sponsorCompany")
     .doc(req.params.companyId)
-    .update(body)
+    .update(res.locals.body as STPMCompaniesUpdateRequest)
     .then(() => {
       res.status(200).send();
       next();
     })
     .catch(errorHandler);
 };
-
 
 const executeIfSponsorMatches: ExpressFunction = (req, res, next) => {
   const errorHandler = expressErrorHandlerFactory(req, res, next);
@@ -44,8 +56,5 @@ const executeIfSponsorMatches: ExpressFunction = (req, res, next) => {
     );
   }
 };
-
-
-
 
 export default updateCompany;
