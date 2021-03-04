ed2654ef8eaf22548ec143b201c378cfc3c2168d
tyler <trode@oakland.edu>
2021-03-03 23:57:45 -0500

    Endpoints for prize module

---
+++ functions/src/modules/stpm/pm/prizes/Category/createCategoryId.ts
@@ -0,0 +1,34 @@
+
+import type { ExpressFunction } from "../../../../../@types";
+import { firebaseApp } from "../../../../../config/firebaseConfig";
+import { expressErrorHandlerFactory } from "../../../../../helpers";
+import { uasPermissionSwitch } from "../../../../../systems/uas";
+
+const createCategoryPrize: ExpressFunction = (req, res, next) => {
+  res.status(200).send();
+  next();
+  uasPermissionSwitch({
+    organizer: { accepted: execute },
+
+  })(req, res, next);
+};
+
+const execute: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+
+
+  const body = JSON.parse(req.body)
+
+  firebaseApp
+    .firestore()
+    .collection("prizeCategoriesPrize")
+    .doc(body.CategoryId)
+    .set(body)
+    .then(() => {
+      res.status(201).send();
+      next();
+    })
+    .catch(errorHandler);
+};
+
+export default createCategoryPrize;
---
+++ functions/src/modules/stpm/pm/prizes/Category/getCategoryId.ts
@@ -0,0 +1,31 @@
+import type { ExpressFunction } from "../../../../../@types";
+import { firebaseApp } from "../../../../../config/firebaseConfig";
+import { expressErrorHandlerFactory } from "../../../../../helpers";
+import { uasPermissionSwitch } from "../../../../../systems/uas";
+
+const getcategoryid: ExpressFunction = (req, res, next) => {
+  res.status(200).send();
+  next();
+  uasPermissionSwitch({
+    organizer: { accepted: execute },
+    sponsor: { accepted: execute },
+  })(req, res, next);
+};
+
+const execute: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+  firebaseApp
+    .firestore()
+    .collection("prizeCategoriesId")
+    .doc(req.params.categoryId)
+    .get()
+    .then((document) => {
+      console.log(req.params.prizeCategoriesId);
+      console.log(document.data());
+      res.status(200).send(document.data());
+      next();
+    })
+    .catch(errorHandler);
+};
+
+export default getcategoryid;
