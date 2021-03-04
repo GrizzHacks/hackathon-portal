286b8a8722f65f583f2ac28f6415e16a60180d57
nicksinishtaj <nicholassinistaj@gmail.com>
2021-03-04 01:14:42 -0500

    demo list

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,23 +1,286 @@
-import React from "react";
-import logo from "./logo.svg";
-import ApiExplorer from "./devTools/ApiExplorer";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from "@material-ui/core/Button";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Checkbox from '@material-ui/core/Checkbox';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Divider from '@material-ui/core/Divider';
 
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+const App: React.FC = () => {
 
-import Home from "./components/Home";
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          width: '100%',
+          maxWidth: '36ch',
+          backgroundColor: theme.palette.background.paper,
+        },
+        inline: {
+          display: 'inline',
+        },
+      }));
 
-const App = () => {
+      const classes = useStyles();
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/api" component={ApiExplorer} />
-          <Route render={() => <div>404 Not Found</div>} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+    <div>
+    <h1>Sponsor Companies</h1>
+    <List className={classes.root}>
+      <ListItem alignItems="flex-start">
+      <ListItemIcon>
+              <Checkbox
+                edge="start"
+                tabIndex={-1}
+                disableRipple
+              />
+            </ListItemIcon>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Amazon"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                AMZ
+              </Typography>
+              {" Gold Tier"}
+            </React.Fragment>
+          }
+        />
+                    <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+      <ListItemIcon>
+              <Checkbox
+                edge="start"
+                tabIndex={-1}
+                disableRipple
+              />
+            </ListItemIcon>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Dynatrace"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                DTR
+              </Typography>
+              {" Gold Tier"}
+            </React.Fragment>
+          }
+        />
+                    <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+      <ListItemIcon>
+              <Checkbox
+                edge="start"
+                tabIndex={-1}
+                disableRipple
+              />
+            </ListItemIcon>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Google"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                GGL
+              </Typography>
+              {" Gold Tier"}
+            </React.Fragment>
+          }
+        />
+                    <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+      <ListItemIcon>
+              <Checkbox
+                edge="start"
+                tabIndex={-1}
+                disableRipple
+              />
+            </ListItemIcon>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Netflix"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                NFLX
+              </Typography>
+              {" Gold Tier"}
+            </React.Fragment>
+          }
+        />
+                    <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+      <ListItemIcon>
+              <Checkbox
+                edge="start"
+                tabIndex={-1}
+                disableRipple
+              />
+            </ListItemIcon>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="General Motors"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                GM
+              </Typography>
+              {" Bronze Tier"}
+            </React.Fragment>
+          }
+        />
+                    <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+      <ListItemIcon>
+              <Checkbox
+                edge="start"
+                tabIndex={-1}
+                disableRipple
+              />
+            </ListItemIcon>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Ford"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                FRD
+              </Typography>
+              {" Gold Tier"}
+            </React.Fragment>
+          }
+        />
+                    <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+      <ListItemIcon>
+              <Checkbox
+                edge="start"
+                tabIndex={-1}
+                disableRipple
+              />
+            </ListItemIcon>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Mongo DB"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                MDB
+              </Typography>
+              {" Gold Tier"}
+            </React.Fragment>
+          }
+        />
+                    <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+      </ListItem>
+    </List>
+    <Divider variant="inset" component="li" />
+    <Button variant="contained" color="primary">
+      Add New Sponsor Company
+    </Button>
+    <Button variant="contained" color="primary">
+      View more details
+    </Button>
+    </div>
+  )
+}
 
-export default App;
+const drawerWidth = 240
+
+export default App
