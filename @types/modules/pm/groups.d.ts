097f890319092b2d2fb15e1ddb8262092f20d7da
Tyler Rode <45582807+trode21@users.noreply.github.com>
2021-03-13 13:53:16 -0500

    Update @types/modules/pm/groups.d.ts
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>

--- @types/modules/pm/groups.d.ts
+++ @types/modules/pm/groups.d.ts
@@ -14,6 +14,6 @@ declare interface PMGroupUpdateRequest {
     prizeCategoryOrder: string[];
   }
 
-//   declare interface PMGroupList {
-//     prizeCategories: STPMCompany[];
-//   }
+declare interface PMGroupList {
+    groups: PMGroup[];
+}
