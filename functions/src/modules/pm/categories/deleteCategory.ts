ed1942c537d34d9769a8e4840bb4e4b247f35b13
Nicholas Sinistaj <51200213+nicksinishtaj@users.noreply.github.com>
2021-03-14 03:16:45 -0400

    feat(api): Create all PM/Category endpoints and datatype (#77)
    * feat(api): Create all PM/Category endpoints and datatype
    
    * Change permissions for executions and validate enum with new enum validators
    
    * Update prizeCategoryId to be categoryId and allow sponsors to create their own category
    
    * Add default value to enum
    
    * revert git ignore
    
    * move category data type to separate folder, update permissions of execution on all endpoints, add approvalStatus enum to category data type, add use case documentation
    
    * add empty string to non-required attributes
    
    * add enum status check to public permissions as well
    
    * fix(functions): Update PM Category Endpoints to Allow Public Access
    
    * fix(functions): Fix PM Category Bugs
    
    Just a few minor updates to fix bugs that slipped through the cracks during the last refactor.
    
    * feat(functions): Lock Sponsor PM Category Edits for Approved Categories
    
    Co-authored-by: Andrew Dimmer <contactandrewd@gmail.com>
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>

--- functions/src/modules/pm/categories/deleteCategory.ts
+++ functions/src/modules/pm/categories/deleteCategory.ts
@@ -1,5 +1,42 @@
-import { ExpressFunction } from "../../../@types";
+import type { ExpressFunction } from "../../../@types";
+import { firebaseApp } from "../../../config/firebaseConfig";
+import { expressErrorHandlerFactory } from "../../../helpers";
+import { uasPermissionSwitch } from "../../../systems/uas";
 
-const replaceMe: ExpressFunction = (req, res, next) => {};
+const deleteCategory: ExpressFunction = (req, res, next) => {
+  uasPermissionSwitch({
+    organizer: { accepted: execute },
+    sponsor: { accepted: executeIfSponsorMatches },
+  })(req, res, next);
+};
 
-export default replaceMe;
+const execute: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+
+  firebaseApp
+    .firestore()
+    .collection("prizeCategories")
+    .doc(req.params.categoryId)
+    .delete()
+    .then(() => {
+      res.status(200).send();
+      next();
+    })
+    .catch(errorHandler);
+};
+
+const executeIfSponsorMatches: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+  const sponsorCompany = (res.locals.permissions as UserPermission).company;
+  if (sponsorCompany === req.params.companyId) {
+    execute(req, res, next);
+  } else {
+    errorHandler(
+      `A sponsor from ${sponsorCompany} tried deleting prizeCategory ${req.params.categoryId}.`,
+      403,
+      "Sorry, you do not have access to perform that operation."
+    );
+  }
+};
+
+export default deleteCategory;
