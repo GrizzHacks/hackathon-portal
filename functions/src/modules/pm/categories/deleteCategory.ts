f6157ed4ec5923c2db97886e72557c64028d8ac6
Andrew Dimmer <contactandrewd@gmail.com>
2021-03-14 04:03:19 -0400

    fix(functions): Fix Check that Sponsors can run PM Category Delete

--- functions/src/modules/pm/categories/deleteCategory.ts
+++ functions/src/modules/pm/categories/deleteCategory.ts
@@ -27,16 +27,38 @@ const execute: ExpressFunction = (req, res, next) => {
 
 const executeIfSponsorMatches: ExpressFunction = (req, res, next) => {
   const errorHandler = expressErrorHandlerFactory(req, res, next);
+
   const sponsorCompany = (res.locals.permissions as UserPermission).company;
-  if (sponsorCompany === req.params.companyId) {
-    execute(req, res, next);
-  } else {
-    errorHandler(
-      `A sponsor from ${sponsorCompany} tried deleting prizeCategory ${req.params.categoryId}.`,
-      403,
-      "Sorry, you do not have access to perform that operation."
-    );
-  }
+
+  firebaseApp
+    .firestore()
+    .collection("prizeCategories")
+    .doc(req.params.categoryId)
+    .get()
+    .then((doc) => {
+      if (
+        sponsorCompany === (doc.data() as PMCategory | undefined)?.companyId
+      ) {
+        if (
+          (doc.data() as PMCategory | undefined)?.approvalStatus !== "approved"
+        ) {
+          execute(req, res, next);
+        } else {
+          errorHandler(
+            `A sponsor from ${sponsorCompany} tried deleting the approved prizeCategory ${req.params.categoryId}.`,
+            400,
+            "Sorry, this prize category has already been approved and published. Reach out to the organizing team if you still want to delete it."
+          );
+        }
+      } else {
+        errorHandler(
+          `A sponsor from ${sponsorCompany} tried deleting prizeCategory ${req.params.categoryId}.`,
+          403,
+          "Sorry, you do not have access to perform that operation."
+        );
+      }
+    })
+    .catch(errorHandler);
 };
 
 export default deleteCategory;
