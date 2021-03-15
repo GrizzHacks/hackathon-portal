801ca217f993ce643b3d8587af99d22a5454e085
tyler <trode@oakland.edu>
2021-03-15 00:18:06 -0400

    pm endpoints

--- functions/src/modules/pm/categories/prizes/createPrize.ts
+++ functions/src/modules/pm/categories/prizes/createPrize.ts
@@ -1,5 +1,53 @@
-import { ExpressFunction } from "../../../../@types";
+import type { ExpressFunction } from "../../../../@types";
+import { firebaseApp } from "../../../../config/firebaseConfig";
+import {
+    expressErrorHandlerFactory,
+    requestBodyTypeValidator,
+  } from "../../../../helpers";
+import { uasPermissionSwitch } from "../../../../systems/uas";
 
-const replaceMe: ExpressFunction = (req, res, next) => {};
 
-export default replaceMe;
+const createCategory: ExpressFunction = (req, res, next) => {
+  res.status(200).send();
+  next();
+  uasPermissionSwitch({
+    organizer: { accepted: validate},
+  })(req, res, next);
+};
+
+const validate: ExpressFunction = (req, res, next) => {
+    const validationRules: ValidatorObjectRules = {
+      type: "object",
+      rules: {
+        prizeId: { rules: ["string"], required: true },
+        prizeName: { rules: ["string"], required: true },
+        PrizeUrl: { rules: ["string"]},
+        prizeCategory: { rules: ["string"]},
+        prizeListingName: { rules: ["string"], required: true },
+        //overriddenBenefits: { [key: string]: string }; TODO: Support Dictionary Advanced Types
+      },
+    };
+    requestBodyTypeValidator(req, res, next)(validationRules, execute);
+  };
+
+
+const execute: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+
+
+  const body = JSON.parse(req.body)
+
+  firebaseApp
+    .firestore()
+    .collection("prizeCategories")
+    .doc(body.CategoryId)
+    .set(body)
+    .then(() => {
+      res.status(201).send();
+      next();
+    })
+    .catch(errorHandler);
+};
+
+
+export default createCategory;
--- functions/src/modules/pm/categories/prizes/updatePrize.ts
+++ functions/src/modules/pm/categories/prizes/updatePrize.ts
@@ -1,5 +1,71 @@
-import { ExpressFunction } from "../../../../@types";
+import type { ExpressFunction } from "../../../../@types";
+import { firebaseApp } from "../../../../config/firebaseConfig";
+import {
+    expressErrorHandlerFactory,
+    requestBodyTypeValidator,
+  } from "../../../../helpers";
+import { uasPermissionSwitch } from '../../../../systems/uas';
 
-const replaceMe: ExpressFunction = (req, res, next) => {};
 
-export default replaceMe;
+
+
+
+const updatePrize: ExpressFunction = (req, res, next) => {
+  res.status(200).send();
+  next();
+  uasPermissionSwitch({
+    organizer: { accepted: validate},
+    // sponsor: { accepted: executeIfSponsorMatches },
+  })(req, res, next);
+};
+
+const validate: ExpressFunction = (req, res, next) => {
+    const validationRules: ValidatorObjectRules = {
+      type: "object",
+      rules: {
+        prizeName: { rules: ["string"], required: true },
+        PrizeUrl: { rules: ["string"]},
+        prizeCategory: { rules: ["string"]},
+        prizeListingName: { rules: ["string"], required: true },
+        //overriddenBenefits: { [key: string]: string }; TODO: Support Dictionary Advanced Types
+      },
+    };
+    requestBodyTypeValidator(req, res, next)(validationRules, execute);
+  };
+
+
+const execute: ExpressFunction = (req, res, next) => {
+
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+
+  const body = JSON.parse(req.body)
+
+  firebaseApp
+    .firestore()
+    .collection("UpdatePrize")
+    .doc(req.params.prizeId)
+    .update(body)
+    .then(() => {
+      res.status(200).send();
+      next();
+    })
+    .catch(errorHandler);
+};
+
+
+
+// const executeIfSponsorMatches: ExpressFunction = (req, res, next) => {
+//   const errorHandler = expressErrorHandlerFactory(req, res, next);
+//   const prizeCategories = (res.locals.permissions as UserPermission).company;
+//   if (prizeCategories === req.params.prizeId) {
+//     execute(req, res, next);
+//   } else {
+//     errorHandler(
+//       `A sponsor from ${prizeCategories} tried viewing benefits for ${req.params.prizeId}.`,
+//       403,
+//       "Sorry, you do not have access to perform that operation."
+//     );
+//   }
+// };
+
+export default updatePrize;
