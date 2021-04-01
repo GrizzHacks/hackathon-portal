b1f59c4fc9dba3cf8f6795d6aa78bc48afe800f1
ejub.has <ejub983@gmail.com>
2021-03-31 21:44:54 -0400

    stray docs: prize endpoints

--- functions/src/modules/pm/categories/prizes/deletePrize.ts
+++ functions/src/modules/pm/categories/prizes/deletePrize.ts
@@ -1,36 +1,5 @@
-import type { ExpressFunction } from "../../../../@types";
-import { firebaseApp } from "../../../../config/firebaseConfig";
-import { expressErrorHandlerFactory,  requestBodyTypeValidator} from "../../../../helpers";
-import { uasPermissionSwitch } from "../../../../systems/uas";
+import { ExpressFunction } from "../../../../@types";
 
-const deleteCatagoryAndPrizeId: ExpressFunction = (req, res, next) => {
-    uasPermissionSwitch({
-        organizer: {accepted: validate},
-    })(req, res, next);
-  };
+const replaceMe: ExpressFunction = (req, res, next) => {};
 
-  const validate: ExpressFunction = (req,res,next) => {
-    const validationRules: ValidatorObjectRules = {
-      type: "object",
-      rules: {
-        prizeCatagoryName: {rules: ["string"]},
-        prizeCategoryDescription: {rules: ["string"]},
-        //prizeCategoryOrder: {rules: ["Array<string>"]}, // TODO support advance types
-      }
-    }
-    requestBodyTypeValidator(req,res,next)((validationRules), execute);
-  }
-
-const execute: ExpressFunction = (req, res, next) => {
-    const errorHandler = expressErrorHandlerFactory(req, res, next);
-    firebaseApp
-    .firestore()
-    .collection("PrizeCatagoryGroup")
-    .doc(req.params.id)
-    .delete()
-    .then(() => {
-        res.status(200).send();
-    })
-    .catch(errorHandler)
-  };
-export default deleteCatagoryAndPrizeId;
+export default replaceMe;
