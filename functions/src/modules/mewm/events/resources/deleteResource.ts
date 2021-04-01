cbc7bb141e5e3014b11acfe293eb4a016a22e72d
ejub.has <ejub983@gmail.com>
2021-04-01 00:00:09 -0400

    endpoints tested for resource endpoints

--- functions/src/modules/mewm/events/resources/deleteResource.ts
+++ functions/src/modules/mewm/events/resources/deleteResource.ts
@@ -1,5 +1,30 @@
-import { ExpressFunction } from "../../../../@types";
+import type { ExpressFunction } from "../../../../@types";
+import { firebaseApp } from "../../../../config/firebaseConfig";
+import { expressErrorHandlerFactory } from "../../../../helpers";
+import { uasPermissionSwitch } from "../../../../systems/uas";
 
-const replaceMe: ExpressFunction = (req, res, next) => {};
 
-export default replaceMe;
+const deleteResource: ExpressFunction = (req, res, next) => {
+  uasPermissionSwitch({
+    organizer: { accepted: execute },
+  })(req, res, next);
+};
+
+const execute: ExpressFunction = (req, res, next) => {
+    const errorHandler = expressErrorHandlerFactory(req, res, next);
+  
+    firebaseApp
+      .firestore()
+      .collection("events")
+      .doc(req.params.eventId)
+      .collection("resources")
+      .doc(req.params.resourceId)
+      .delete()
+      .then(() => {
+        res.status(200).send();
+        next();
+      })
+      .catch(errorHandler);
+  };
+
+export default deleteResource;
--- functions/src/modules/mewm/events/resources/updateResource.ts
+++ functions/src/modules/mewm/events/resources/updateResource.ts
@@ -1,5 +1,60 @@
-import { ExpressFunction } from "../../../../@types";
+import type { ExpressFunction } from "../../../../@types";
+import { firebaseApp } from "../../../../config/firebaseConfig";
+import {
+  expressErrorHandlerFactory,
+  requestBodyTypeValidator,
+} from "../../../../helpers";
+import { uasPermissionSwitch } from "../../../../systems/uas";
 
-const replaceMe: ExpressFunction = (req, res, next) => {};
+const updateResources: ExpressFunction = (req, res, next) => {
+  uasPermissionSwitch({
+    organizer: { accepted: validate },
+    sponsor: { accepted: executeIfSponsorMatches },
+  })(req, res, next);
+};
 
-export default replaceMe;
+const validate: ExpressFunction = (req, res, next) => {
+  const validationRules: ValidatorObjectRules = {
+    type: "object",
+    rules: {
+        resourceName: { rules: ["string"] },
+        resourceDescription: { rules: ["string"] },
+        resourceUrl: { rules:  ["string"] },
+    },
+  };
+  requestBodyTypeValidator(req, res, next)(validationRules, execute);
+};
+
+
+const execute: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+  const body = res.locals.parsedBody as MEWMEventResourcesUpdateRequest;
+  firebaseApp
+    .firestore()
+    .collection("events")
+    .doc(req.params.eventId)
+    .collection("resources")
+    .doc(req.params.resourceId)
+    .update(body)
+    .then(() => {
+      res.status(200).send();
+      next();
+    })
+    .catch(errorHandler);
+};
+
+const executeIfSponsorMatches: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+  const sponsorCompany = (res.locals.permissions as UserPermission).companyId;
+  if (sponsorCompany === req.params.companyId) {
+    validate(req, res, next);
+  } else {
+    errorHandler(
+      `A sponsor from ${sponsorCompany} tried updating resources for ${req.params.resourceId}.`,
+      403,
+      "Sorry, you do not have access to perform that operation."
+    );
+  }
+};
+
+export default updateResources;
