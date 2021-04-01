2332f54c46ba8e0625acc08ea42e6fcd74314545
ejub.has <ejub983@gmail.com>
2021-03-31 23:55:37 -0400

    data types

--- functions/src/modules/urm/questions/listQuestions.ts
+++ functions/src/modules/urm/questions/listQuestions.ts
@@ -1,5 +1,31 @@
-import { ExpressFunction } from "../../../@types";
+import type { ExpressFunction } from "../../../@types";
+import { firebaseApp } from "../../../config/firebaseConfig";
+import { expressErrorHandlerFactory } from "../../../helpers";
 
-const replaceMe: ExpressFunction = (req, res, next) => {};
+const listQuestions: ExpressFunction = (req, res, next) => {
+    const errorHandler = expressErrorHandlerFactory(req, res, next);
+  
+    firebaseApp
+      .firestore()
+      .collection("questions")
+      .orderBy("applicationQuestionId", "asc")
+      .where("values", "==" , res.locals.parsedBody.values)
+      .get()
+      .then((documents) => {
+        send(documents.docs)(req, res, next);
+      })
+      .catch(errorHandler);
+  };
 
-export default replaceMe;
+  const send: (
+    docs: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[]
+  ) => ExpressFunction = (docs) => (req, res, next) => {
+    const urmquestions: URMQuestion[] = [];
+    for (const doc of docs) {
+      urmquestions.push(doc.data() as URMQuestion);
+    }
+    res.status(200).send(JSON.stringify({ urmquestions } as URMQuestionsList));
+    next();
+  };
+
+export default listQuestions;
