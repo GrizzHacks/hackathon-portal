8a88c5cc3618aa2deac3e913c82df22178296b3f
Tyler Rode <45582807+trode21@users.noreply.github.com>
2021-03-31 21:45:38 -0400

    feat(functions): Create MEWM Time Slot API Endpoints (#81)
    * MEWM timeslots endpoints
    
    * timeslots endpoints
    
    * update on timeSlot endpoint and types
    
    * update on all endpoints
    
    * update on types
    
    * update on list
    
    * Update @types/modules/mewm/timeslots.d.ts
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: tyler <trode@oakland.edu>
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>

--- functions/src/modules/mewm/timeslots/deleteTimeslot.ts
+++ functions/src/modules/mewm/timeslots/deleteTimeslot.ts
@@ -1,5 +1,27 @@
-import { ExpressFunction } from "../../../@types";
+import type { ExpressFunction } from "../../../@types";
+import { firebaseApp } from "../../../config/firebaseConfig";
+import { expressErrorHandlerFactory } from "../../../helpers";
+import { uasPermissionSwitch } from "../../../systems/uas";
 
-const replaceMe: ExpressFunction = (req, res, next) => {};
+const deleteTimeslot: ExpressFunction = (req, res, next) => {
+  uasPermissionSwitch({
+    organizer: { accepted: execute },
+  })(req, res, next);
+};
 
-export default replaceMe;
+const execute: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+
+  firebaseApp
+    .firestore()
+    .collection("timeslots")
+    .doc(req.params.timeslotId)
+    .delete()
+    .then(() => {
+      res.status(200).send();
+      next();
+    })
+    .catch(errorHandler);
+};
+
+export default deleteTimeslot;
