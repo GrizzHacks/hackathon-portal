1c62a351f5eda33337bfafa886809ab98605afc1
Nicholas Sinistaj <51200213+nicksinishtaj@users.noreply.github.com>
2021-04-11 21:11:52 -0400

    feat(api): Create all URM/profile endpoints and datatype (#109)
    * create endpoints, update index.ts file
    
    * Update data type attributes to correspond to documentation update
    
    * remove otherQuestions attribute from data type

--- functions/src/modules/urm/profiles/deleteProfiles.ts
+++ functions/src/modules/urm/profiles/deleteProfiles.ts
@@ -1,5 +1,27 @@
-import { ExpressFunction } from "../../../@types";
+import type { ExpressFunction } from "../../../@types";
+import { firebaseApp } from "../../../config/firebaseConfig";
+import { expressErrorHandlerFactory } from "../../../helpers";
+import { uasPermissionSwitch } from "../../../systems/uas";
 
-const replaceMe: ExpressFunction = (req, res, next) => {};
+const deleteProfile: ExpressFunction = (req, res, next) => {
+  uasPermissionSwitch({
+    organizer: { accepted: execute },
+  })(req, res, next);
+};
 
-export default replaceMe;
+const execute: ExpressFunction = (req, res, next) => {
+  const errorHandler = expressErrorHandlerFactory(req, res, next);
+
+  firebaseApp
+    .firestore()
+    .collection("profiles")
+    .doc(req.params.profileId)
+    .delete()
+    .then(() => {
+      res.status(200).send();
+      next();
+    })
+    .catch(errorHandler);
+};
+
+export default deleteProfile;
--- functions/src/modules/urm/profiles/index.ts
+++ functions/src/modules/urm/profiles/index.ts
@@ -2,13 +2,13 @@ import type { Express } from "express";
 import createProfiles from "./createProfiles";
 import deleteProfiles from "./deleteProfiles";
 import getProfiles from "./getProfiles";
-// import listTProfiles from "./listProfiles";
+import listProfiles from "./listProfiles";
 import updateProfiles from "./updateProfiles";
 
 export default (app: Express, endpoint: string) => {
-//   app.get(`${endpoint}/`, listProfiles);
   app.get(`${endpoint}/:profilesId`, getProfiles);
   app.post(`${endpoint}/`, createProfiles);
   app.patch(`${endpoint}/:profilesId`, updateProfiles);
   app.delete(`${endpoint}/:profilesId`, deleteProfiles);
+  app.get(`${endpoint}/`, listProfiles);
 };
