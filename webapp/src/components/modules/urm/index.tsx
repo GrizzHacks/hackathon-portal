34451f79e9bbb6c4270fa904aebc51cdc6802f99
Ejub Hasecic <45454572+ejub-has@users.noreply.github.com>
2021-04-26 20:55:56 -0400

    Update index.tsx

--- webapp/src/components/modules/urm/index.tsx
+++ webapp/src/components/modules/urm/index.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Error404Page from "../../pages/Error404Page";
 import RulesPages from "./RulesPages";
-import QuestionPages from "./QuestionPages";
 
 const URMPages: React.FunctionComponent = () => {
   return (
     <Switch>
       <Route path="/urm/rules" component={RulesPages} />
-      <Route path="/urm/questions" component={QuestionPages} />
       <Route component={Error404Page} />
     </Switch>
   );
